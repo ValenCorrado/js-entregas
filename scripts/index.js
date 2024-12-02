0cac4415dcd1367301877515b66c08180f62a638
ValenCorrado <valentincorrado2005@gmail.com>
2024-12-02 18:48:45 -0300

    subida de preentrega

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+let secretNumber = Math.floor (Math.random() * 100) + 1 /* Esto crea un numero aleatorio del 1 al 100 */
+
+function startGame(){
+let number;
+let intentos = 0;
+
+while (number != secretNumber){
+    number = parseInt(prompt("Adivina el numero (del 1 al 100), intenta lograrlo con la menor cantidad de intentos"));
+    intentos++;
+    if ( number < secretNumber)
+        alert ("Demasiado bajo, prueba con uno mas alto")
+    else if (number > secretNumber)
+        alert ("Demasiado alto, prueba con uno mas bajo")
+} 
+alert("¡Muy bienn! Lograste adivinar el numero en tan solo " + intentos + " intentos y el numero era " + secretNumber + ".")
+}
+document.getElementById('game-number').addEventListener('click', startGame);   
